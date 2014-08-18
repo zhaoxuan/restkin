f743ee5cc7c6d044b08707b503b791e82b0231f2
zhaoxuan <zhaoxuan1727@gmail.com>
2014-08-18 10:10:33 +0800

    fix bug

--- example/restkin_server.sh
+++ example/restkin_server.sh
@@ -3,5 +3,5 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd $DIR/../
 echo 'Start restkin server at 6956'
 echo 'Connect to zipkin scribe server at localhost:9410'
-echo 'More info: ./bin/restkin-api -help'
+echo 'More info: ./bin/restkin-api --help'
 ./bin/restkin-api --scribe=tcp:localhost:9410 --trace=10:10
