ffc30e671c741fbb5d54d6ff5e3f1168517306b3
zhaoxuan <zhaoxuan1727@gmail.com>
2014-08-21 18:52:50 +0800

    add X-B3-Category to restkin

--- example/post_data.sh
+++ example/post_data.sh
@@ -3,4 +3,4 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 echo 'Generate json post data'
 python $DIR/generate_data.py
 echo 'Post example/json_data to restkin server'
-curl -H "Content-Type: application/json" -X POST -d @$DIR/json_data http://localhost:6956/v1.0/trace
+curl -H "Content-Type: application/json" -H "X-B3-Category:test" -X POST -d @$DIR/json_data http://localhost:6956/v1.0/trace
